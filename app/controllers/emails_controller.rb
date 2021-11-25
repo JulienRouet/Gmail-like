668f54b24248d0c67cbc680e8bbb006deac4ca5d
JulienRouet <julienrouet@live.fr>
2021-11-25 20:35:19 +0100

    2_4

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,13 +1,27 @@
 class EmailsController < ApplicationController
   def index
     @emails = Email.all
-    @emails = @emails.sort_by {|email| email.id}
+  end
+
+  def create
+    @email = Email.create(object: Faker::Book.title, body: Faker::Lorem.paragraph_by_chars)
+
+    respond_to do |format|
+      format.html {redirect_to emails_path}
+      format.js { }
+    end
   end
 
   def destroy
   end
 
   def show
+    @email = Email.find(params[:id])
+
+    respond_to do |format|
+      format.html {redirect_to email_path(@email)}
+      format.js { }
+    end
   end
 
   def update
